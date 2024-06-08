fc2c4c33cbeaa67dc59445b6da8ef2c8c1fca4f5
haron <haronxlee@gmail.com>
2024-06-08 23:10:45 +0900

    Feat: app module import image module in app module change root url of mongoose module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,17 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+import { ImageModule } from '@image/image.module';
 
 @Module({
-	imports: [MongooseModule.forRoot('mongodb://localhost/nest')],
+	imports: [
+		ConfigModule.forRoot({
+			isGlobal: true,
+		}),
+		MongooseModule.forRoot(process.env.DB_URL),
+		ImageModule,
+	],
 	controllers: [AppController],
 	providers: [AppService],
 })
