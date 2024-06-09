98848dd048321b073ba882c4badfdd24efe14652
bringvotrevin <bringvotrevin@gmail.com>
2024-06-10 03:50:40 +0900

    Feat: add jwtStrategy guard

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,15 +3,24 @@ import { AuthService } from './auth.service';
 import { UsersModule } from './test/users.module';
 import { PassportModule } from '@nestjs/passport';
 import { LocalStrategy } from './local.strategy';
+import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './jwt.strategy';
 import { AuthController } from './auth.controller';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
 @Module({
-	imports: [UsersModule, PassportModule],
+	imports: [
+		UsersModule,
+		PassportModule,
+		JwtModule.register({
+			secret: process.env.JWT_SECRET,
+			signOptions: { expiresIn: '30m' },
+		}),
+	],
 	controllers: [AuthController],
-	providers: [AuthService, LocalStrategy],
+	providers: [AuthService, LocalStrategy, JwtStrategy],
 	exports: [AuthService],
 })
 export class AuthModule {}
