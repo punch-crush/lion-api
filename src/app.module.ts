16fd5018fcbbfe1e9c5e61a82f6016999dcc5b94
d-charlie-kim <d.charlie.kim@gmail.com>
2024-06-10 21:28:19 +0900

    feat: Mongoose Module 세팅

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,19 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+import { UpdateProfileModule } from '@user/updateProfile.module';
+// import { ImageModule } from '@image/image.module';
 
 @Module({
-	imports: [MongooseModule.forRoot('mongodb://localhost/nest')],
+	imports: [
+		ConfigModule.forRoot({
+			isGlobal: true,
+		}),
+		MongooseModule.forRoot(process.env.DB_URL),
+		UpdateProfileModule,
+		// ImageModule,
+	],
 	controllers: [AppController],
 	providers: [AppService],
 })
