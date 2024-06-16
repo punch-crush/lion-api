4b33b6d4648fe51c787c0bb06e135eb366e3b8e3
bringvotrevin <bringvotrevin@gmail.com>
2024-06-16 15:05:04 +0900

    Feat: add configuration file for managing env variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,17 +6,22 @@ import { ImageModule } from '@image/image.module';
 import { APP_PIPE, RouterModule } from '@nestjs/core';
 import { routes } from './app.routes';
 import { UserModule } from '@user/user.module';
+import { AuthModule } from './auth/auth.module';
+import configuration from '@config/configuration';
 
 @Module({
 	imports: [
 		ConfigModule.forRoot({
 			isGlobal: true,
+			load: [configuration],
+			cache: true,
 		}),
 		MongooseModule.forRoot(process.env.DB_URL),
 		RouterModule.register(routes),
 		ImageModule,
 		UserModule,
 		ImageModule,
+		AuthModule,
 	],
 	controllers: [AppController],
 	providers: [
