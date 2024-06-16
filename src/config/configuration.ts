fe0b949b0879e47f7516c8b5c763b04a6a61e956
bringvotrevin <bringvotrevin@gmail.com>
2024-06-17 01:17:52 +0900

    Remove: configuration file

--- src/config/configuration.ts
+++
@@ -1,9 +0,0 @@
-export default () => ({
-	port: parseInt(process.env.PORT, 10) || 8080,
-	database: {
-		host: process.env.DB_URL + process.env.DB_DATABASE,
-	},
-	jwt: {
-		secretKey: process.env.JWT_SECRET,
-	},
-});
