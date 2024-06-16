e8ef2a94ae8a43000dca08fe8dc201ad310a3508
bringvotrevin <bringvotrevin@gmail.com>
2024-06-16 16:35:08 +0900

    Feat: add findUser to user.service, fix validation in auth.service

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,18 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { UsersModule } from './test/users.module';
-import { PassportModule } from '@nestjs/passport';
 import { LocalStrategy } from './local.strategy';
 import { JwtModule } from '@nestjs/jwt';
 import { JwtStrategy } from './jwt.strategy';
 import { AuthController } from './auth.controller';
-import dotenv from 'dotenv';
-
-dotenv.config();
+import { UserModule } from '@user/user.module';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
 	imports: [
-		UsersModule,
+		UserModule,
 		PassportModule,
 		JwtModule.register({
 			secret: process.env.JWT_SECRET,
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,22 +1,17 @@
 import { Injectable } from '@nestjs/common';
-import { UsersService } from './test/users.service';
 import { JwtService } from '@nestjs/jwt';
+import { UserService } from '@user/user.service';
 
 @Injectable()
 export class AuthService {
 	constructor(
-		private usersService: UsersService,
+		private usersService: UserService,
 		private jwtService: JwtService,
 	) {}
 
 	async validateUser(email: string, password: string): Promise<any> {
-		const user = await this.usersService.findOne(email);
-		if (user && user.password === password) {
-			const { password, ...result } = user;
-			void password;
-			return result;
-		}
-		return null;
+		const user = await this.usersService.findUser(email, password);
+		return user;
 	}
 
 	async login(user: any) {
