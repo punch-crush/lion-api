22fae372bd1fcb6910b5a341e6bd442c41dc06c2
haron <haronxlee@gmail.com>
2024-06-17 00:59:55 +0900

    Feat: add ProductDTO and modify error message

--- src/product/product.dto.ts
+++ src/product/product.dto.ts
@@ -1,13 +1,41 @@
-export interface ProductDTO {
-	readonly id: string;
+import { IsString, IsNumber, IsNotEmpty, ValidateNested, IsArray } from 'class-validator';
+import { ProfileResponse } from '@user/dto/user-base.dto';
+import { Type } from 'class-transformer';
+
+export class Product {
+	@IsString()
+	@IsNotEmpty({ message: '필수 입력사항을 입력해주세요' })
 	itemName: string;
+
+	@IsNumber({}, { message: '가격은 숫자로 입력해야 합니다.' })
+	@IsNotEmpty({ message: '필수 입력사항을 입력해주세요' })
 	price: number;
 	link: string;
 	itemImage: string;
-	authorId: string;
 }
 
-export interface ProductListDTO {
-	data: number;
-	product: ProductDTO[];
+export class CreateProductDTO {
+	@ValidateNested()
+	@Type(() => Product)
+	product: Product;
+}
+
+export class UpdateProductDTO extends CreateProductDTO {}
+
+export class InProductResponse extends Product {
+	@ValidateNested()
+	@Type(() => ProfileResponse)
+	author: ProfileResponse;
+}
+
+export class ProductResponse {
+	product: InProductResponse;
+}
+
+export class ProductListDTO {
+	@IsNumber()
+	data: number = 0;
+
+	@IsArray()
+	product: InProductResponse[];
 }
