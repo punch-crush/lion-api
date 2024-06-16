ffef0f5b2bc25f192bbb6bdd259ef86665fb5c32
haron <haronxlee@gmail.com>
2024-06-16 22:30:44 +0900

    Feat: add product path in app.routes

--- src/app.routes.ts
+++ src/app.routes.ts
@@ -1,10 +1,14 @@
 import { AppModule } from './app.module';
 import { ImageModule } from '@image/image.module';
+import { ProductModule } from '@product/product.module';
 
 export const routes = [
 	{
 		path: '/',
 		module: AppModule,
-		children: [{ path: '/image', module: ImageModule }],
+		children: [
+			{ path: '/image', module: ImageModule },
+			{ path: '/product', module: ProductModule },
+		],
 	},
 ];
