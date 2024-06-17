8df7b4f299d882ed986181c187cfcdd59c959cb9
d-charlie-kim <d.charlie.kim@gmail.com>
2024-06-17 21:15:46 +0900

    feat: follow unfollow 구현, 적절한 return, isfollow 업데이트, 팔로우 한사람 following 리스트 업데이트, 팔로우 당한사람 Follower 리스트 업데이트

--- src/user/profile.controller.ts
+++ src/user/profile.controller.ts
@@ -38,14 +38,69 @@ export class ProfileController {
 
 	//팔로우
 	@Post(':accountname/follow')
-	async follow() {}
+	async follow(
+		@Headers('Authorization') authHeader: string,
+		@Param('accountname') accountname: string,
+	) {
+		try {
+			const token = authHeader.split(' ')[1];
+			if (!token) {
+				throw new HttpException('권한이 없습니다.', HttpStatus.UNAUTHORIZED);
+			}
+			const myAccountName = 'newChar'; //토큰 이용해 알아온다.
 
+			if (accountname == myAccountName) {
+				throw new HttpException(
+					'자기 자신을 팔로우 할 수 없습니다.',
+					HttpStatus.INTERNAL_SERVER_ERROR,
+				);
+			}
+
+			return await this.profileService.follow(myAccountName, accountname);
+		} catch (error) {
+			if (error instanceof HttpException) {
+				throw error;
+			} else {
+				throw new HttpException('잘못된 접근입니다.', HttpStatus.INTERNAL_SERVER_ERROR);
+			}
+		}
+	}
+
+	//언팔로우
 	@Delete(':accountname/unfollow')
-	async unfollow() {}
+	async unfollow(
+		@Headers('Authorization') authHeader: string,
+		@Param('accountname') accountname: string,
+	) {
+		try {
+			const token = authHeader.split(' ')[1];
+			if (!token) {
+				throw new HttpException('권한이 없습니다.', HttpStatus.UNAUTHORIZED);
+			}
+			const myAccountName = 'newChar'; //토큰 이용해 알아온다.
 
+			if (accountname == myAccountName) {
+				throw new HttpException(
+					'자기 자신을 언팔로우 할 수 없습니다.',
+					HttpStatus.INTERNAL_SERVER_ERROR,
+				);
+			}
+
+			return await this.profileService.unfollow(myAccountName, accountname);
+		} catch (error) {
+			if (error instanceof HttpException) {
+				throw error;
+			} else {
+				throw new HttpException('잘못된 접근입니다.', HttpStatus.INTERNAL_SERVER_ERROR);
+			}
+		}
+	}
+
+	//팔로잉 리스트
 	@Get(':accountname/following?limit=Number&skip=Number')
 	async getFollowingList() {}
 
+	//팔로워 리스트
 	@Get(':accountname/follower/?limit=Number&skip=Number')
 	async getFollowerList() {}
 }
--- src/user/profile.service.ts
+++ src/user/profile.service.ts
@@ -16,4 +16,48 @@ export class ProfileService {
 			profile: user.readOnlyDataWithFollow,
 		};
 	}
+
+	async follow(myAccountName: string, accountname: string) {
+		const user = await this.userModel.findOne({ accountname });
+		const myProfile = await this.userModel.findOne({ accountname: myAccountName });
+		if (!user || !myProfile) {
+			throw new HttpException('해당 계정이 존재하지 않습니다.', HttpStatus.NOT_FOUND);
+		}
+
+		if (!user.follower.includes(myAccountName)) {
+			user.follower.push(myAccountName);
+			await user.save();
+		}
+		if (!myProfile.following.includes(accountname)) {
+			myProfile.following.push(accountname);
+			await myProfile.save();
+		}
+
+		const userProfile = user.readOnlyDataWithFollow;
+		userProfile.isfollow = userProfile.follower.includes(myAccountName) ? true : false;
+		return {
+			profile: userProfile,
+		};
+	}
+
+	async unfollow(myAccountName: string, accountname: string) {
+		const user = await this.userModel.findOne({ accountname });
+		const myProfile = await this.userModel.findOne({ accountname: myAccountName });
+		if (!user || !myProfile) {
+			throw new HttpException('해당 계정이 존재하지 않습니다.', HttpStatus.NOT_FOUND);
+		}
+
+		if (user.follower.includes(myAccountName)) {
+			user.follower = user.follower.filter(name => name !== myAccountName);
+			await user.save();
+		}
+		if (myProfile.following.includes(accountname)) {
+			myProfile.following = myProfile.following.filter(name => name !== accountname);
+			await myProfile.save();
+		}
+
+		return {
+			profile: user.readOnlyDataWithFollow,
+		};
+	}
 }
