257eb2290a559bde8d2e8d69146c3d934f3e8363
bringvotrevin <bringvotrevin@gmail.com>
2024-06-25 19:54:47 +0900

    Chore: only return _id in jwtGuard

--- src/user/auth/guards/jwt.strategy.ts
+++ src/user/auth/guards/jwt.strategy.ts
@@ -13,6 +13,6 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
 	}
 
 	async validate(payload: any) {
-		return { _id: payload.sub, email: payload.email };
+		return { _id: payload.sub };
 	}
 }
