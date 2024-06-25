26a4be5830c221c1fb5521d8131c0cfffe0e3b97
bringvotrevin <bringvotrevin@gmail.com>
2024-06-25 19:59:37 +0900

    Refactor: use @HandleErrors in login

--- src/user/auth/auth.controller.ts
+++ src/user/auth/auth.controller.ts
@@ -1,40 +1,23 @@
-import {
-	Controller,
-	Get,
-	Headers,
-	Header,
-	HttpException,
-	HttpStatus,
-	Post,
-	Req,
-	UseGuards,
-} from '@nestjs/common';
+import { Controller, Get, Headers, Header, Post, Req, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LocalAuthGuard } from './guards/local-auth.guard';
+import { HandleErrors } from '@util/error-decorator';
 
 @Controller()
 export class AuthController {
 	constructor(private readonly authService: AuthService) {}
 
 	@UseGuards(LocalAuthGuard)
+	@HandleErrors()
 	@Post('user/login')
 	async logIn(@Req() req) {
-		try {
-			const jwt = await this.authService.login(req.user);
-
-			return {
-				user: {
-					...req.user,
-					token: jwt.accessToken,
-				},
-			};
-		} catch (error) {
-			if (error instanceof HttpException) {
-				throw error;
-			} else {
-				throw new HttpException('잘못된 접근입니다.', HttpStatus.INTERNAL_SERVER_ERROR);
-			}
-		}
+		const jwt = await this.authService.login(req.user);
+		return {
+			user: {
+				...req.user,
+				token: jwt.accessToken,
+			},
+		};
 	}
 
 	@Get('user/checktoken')
