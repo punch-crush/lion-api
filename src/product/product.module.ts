ad93751b4d57f2fd45f336efb80580dafae9b029
haron <haronxlee@gmail.com>
2024-06-30 14:39:03 +0900

    Feat: deleteProduct시에 저장된 이미지 삭제 기능 추가

--- src/product/product.module.ts
+++ src/product/product.module.ts
@@ -4,11 +4,13 @@ import { Product, ProductSchema } from './product.schema';
 import { ProductService } from './product.service';
 import { ProductController } from './product.controller';
 import { UserModule } from '@user/user.module';
+import { ImageModule } from '@image/image.module';
 
 @Module({
 	imports: [
 		MongooseModule.forFeature([{ name: Product.name, schema: ProductSchema }]),
 		UserModule,
+		ImageModule,
 	],
 	providers: [ProductService],
 	controllers: [ProductController],
